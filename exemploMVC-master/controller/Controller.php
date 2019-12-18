8e2e4612b1b18d9b2edfb8d78523dffb6a76fd52
WillianK26 <54908106+WillianK26@users.noreply.github.com>
2019-12-18 08:27:31 -0300

    Add files via upload

---
+++ exemploMVC-master/controller/Controller.php
@@ -0,0 +1,65 @@
+<?php
+
+include_once("model/Model.php");
+
+class Controller {
+
+    public $model;
+
+    public function __construct() {
+        $this->model = new Model();
+    }
+
+    public function invoke($acao = 'home') {
+        $reslt = $this->model->getLogin();
+
+        switch ($reslt) {
+            case 'login':
+                $acao = 'logado';
+                break;
+        }
+        // Chama a funcao getLogin() da classe Model
+        // e armazena o valor de retorno dessa funcao
+        // na variavel reslt.
+        switch ($acao) {
+            case 'home':
+                $this->showConteudoInicialView();
+                break;
+            case 'login':
+                $this->showTelaLoginView();
+                break;
+            case 'logout':
+                $this->showTelaDeLogoutView();
+                break;
+            case 'logado':
+                $this->showTelaDeLogadoView();
+                break;
+            case 'perfil':
+                $this->showTelaDePerfilView();
+                break;
+        }
+    }
+    
+    private function showConteudoInicialView(){
+        include 'view/conteudoInicialView.php';
+    }
+    
+    private function showTelaLoginView(){
+        include 'view/telaLoginView.php';
+    }
+    
+    private function showTelaDeLogoutView(){
+        $custom = "ahahaha"; //mysql_
+        include 'view/telaLogoutView.php';
+    }
+
+    private function showTelaDeLogadoView(){
+        include 'view/TelaLogado.php';
+    }
+    private function showTelaDePerfilView(){
+        include 'view/TelaPerfil.php';
+    }
+    
+}
+
+?>
---
+++ exemploMVC-master/view/TelaLogado.php
@@ -0,0 +1,5 @@
+<br>
+Bem Vindo(a). Voce esta logado
+<a href="?acao=logout">Deslogar</a>
+
+<a href="?acao=perfil">Perfil</a>
